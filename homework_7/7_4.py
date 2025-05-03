292ec8294162aadcdb90e5e89df62dea603295d0
Shovkoplias <ijdrjgkzcdima@gmail.com>
2025-05-03 17:45:40 +0300

    ended hw 7

---
+++ homework_7/7_4.py
@@ -0,0 +1,15 @@
+def common_elements():
+    first_range = range(3, 100, 3)
+    first_numbers = {0}
+    for i in first_range:
+        first_numbers.add(i)
+
+    second_range = range(5, 100, 5)
+    second_numbers = {0}
+    for i in second_range:
+        second_numbers.add(i)
+    return first_numbers&second_numbers
+
+
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+
